00e5c61443e54ffe3ea27f76354d63b3b875c2e9
Hays Rs <wonderhays@gmail.com>
2021-10-26 13:50:00 +0300

    bid fix

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,20 @@ async fn main() {
     let settings = Settings::from_file("./settings.json".into()).await;
     let server_sessions_list = Arc::new(SessionList::new());
 
+
+    tokio::spawn(start(
+        SocketAddr::from(([0, 0, 0, 0], 8080)),
+        server_sessions_list.clone(),
+        metrics.clone(),
+    ));
+    tokio::spawn(http_start(
+        SocketAddr::from(([0, 0, 0, 0], 8081)),
+        metrics.clone(),
+    ));
+
     if settings.target_exchange == "ftx" {
-        let ctx = FtxExchangeContext::new_by_settings(&settings);
         let mut binance_socket =
-            ExchangeWebscoket::new(ctx);
+            ExchangeWebscoket::new(FtxExchangeContext::new_by_settings(&settings));
         let handler = binance_socket.get_subscribe();
 
         tokio::spawn(handle_and_process_bidask(
@@ -36,16 +46,6 @@ async fn main() {
         ));
 
         tokio::spawn(ftx_socket.start(metrics.clone()));
-
-        tokio::spawn(start(
-            SocketAddr::from(([0, 0, 0, 0], 8080)),
-            server_sessions_list.clone(),
-            metrics.clone(),
-        ));
-        tokio::spawn(http_start(
-            SocketAddr::from(([0, 0, 0, 0], 8081)),
-            metrics.clone(),
-        ));
     }
 
     loop {
